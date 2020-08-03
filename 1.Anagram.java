7f69bdb7b46c51326e03b54444565683f4e1973c
Mazbah <mazbah142@gmail.com>
2020-08-03 09:52:34 +0600

    Create 1.Anagram.java

---
+++ 1.Anagram.java
@@ -0,0 +1,30 @@
+// Question-1: Write a function that detects if two strings are anagram e.g. ‘bleat’ and ‘table’ are anagrams but ‘eat’ and ‘tar’ are not.
+// Solution:
+
+class Anagram {
+
+    boolean isAnagram(String Word1, String Word2) {
+
+        if (Word1.length() != Word2.length()) {
+            return false;
+        }
+
+        Word1 = Word1.toLowerCase();
+        Word2 = Word2.toLowerCase();
+
+        int[] counT = new int[128]; 
+
+        for (int i = 0; i < Word1.length(); i++) {
+            counT[Word1.charAt(i)]++;
+            counT[Word2.charAt(i)]--;
+        }
+
+        //If they are Anagram then all char count must be zero.
+        for (int i = 0; i < 128; i++) {
+            if (counT[i] != 0)
+                return false;
+        }
+
+        return true;
+    }
+}
