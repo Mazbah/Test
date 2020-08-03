dc08e1a1994c99e524f33b218550c8ad1dc55edb
Mazbah <mazbah142@gmail.com>
2020-08-03 10:35:14 +0600

    Create Plane.java

---
+++ Plane.java
@@ -0,0 +1,63 @@
+package BongoBD;
+
+/**
+ * Question-2:
+ * 2) Explain the design pattern used in following:
+ *  interface Vehicle {
+ *    int set_num_of_wheels()
+ *    int set_num_of_passengers()
+ *    boolean has_gas()
+ *  }
+ * b) Use a different design pattern for this solution.
+ */
+
+public class Plane {
+    private int wheels;
+    private int passengers;
+    private boolean hasGas;
+
+    private Plane(int wheels, int passengers, boolean hasGas) {
+        this.wheels = wheels;
+        this.passengers = passengers;
+        this.hasGas = hasGas;
+    }
+
+    public int get_num_of_wheels() {
+        return wheels;
+    }
+
+
+    public int get_num_of_passengers() {
+        return passengers;
+    }
+
+
+    public boolean has_gas() {
+        return hasGas;
+    }
+
+    public static class Builder {
+        private int wheels;
+        private int passengers;
+        private boolean hasGas;
+
+        public Builder setWheels(int wheels) {
+            this.wheels = wheels;
+            return this;
+        }
+
+        public Builder setPassengers(int passengers) {
+            this.passengers = passengers;
+            return this;
+        }
+
+        public Builder setHasGas(boolean hasGas) {
+            this.hasGas = hasGas;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(wheels, passengers, hasGas);
+        }
+    }
+}
