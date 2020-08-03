a1a6977bce208984b90ef30034466086b01dc2a2
Mazbah <mazbah142@gmail.com>
2020-08-03 10:01:39 +0600

    Create AnagramUnitTest.java

---
+++ AnagramUnitTest.java
@@ -0,0 +1,24 @@
+package BongoBD;
+
+import org.junit.Test;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class AnagramUnitTest {
+
+	@Test
+    public void AnagramTest() {
+        Anagram mAnagram = new Anagram();
+        //Given test Case
+        assertTrue(mAnagram.isAnagram("bleat", "table"));
+        assertFalse(mAnagram.isAnagram("eat", "tar"));
+
+        assertTrue(mAnagram.isAnagram("mazbah uddin", "niddu habzam"));
+        assertTrue(mAnagram.isAnagram("KEY", "EYK"));
+        assertFalse(mAnagram.isAnagram("ans", "sab"));
+        assertTrue(mAnagram.isAnagram("java", "avja"));
+        assertTrue(mAnagram.isAnagram("Code", "edoC"));
+        assertFalse(mAnagram.isAnagram("bongo", "gonba"));
+        assertFalse(mAnagram.isAnagram("Android", "droid"));
+    }
+}
